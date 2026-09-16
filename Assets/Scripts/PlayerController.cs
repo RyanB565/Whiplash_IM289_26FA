e783e914ad34ee5535a46f12fbd75f2d47afded8
TrailMix36 <sbrueningwright@mail.bradley.edu>
2026-09-16 11:55:11 -0500

    SoulAttackFramework

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
     private Vector2 moveDir;
     private Rigidbody2D rb2d;
     [SerializeField] private float moveSpeed;
+    [SerializeField] private GameObject SoulAttack;
+    private GameObject SoulSpawn;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -23,4 +25,20 @@ public class PlayerController : MonoBehaviour
     {
         rb2d.linearVelocity = new Vector2(moveDir.x * moveSpeed, moveDir.y * moveSpeed);
     }
+
+    public void Soul()
+    {
+        SoulSpawn =  Instantiate(SoulAttack, transform);
+        rb2d.constraints = RigidbodyConstraints2D.FreezePosition;
+    }
+
+    public void SoulDestroy()
+    {
+        if(SoulSpawn != null)
+        {
+            Destroy(SoulSpawn);
+            rb2d.constraints = RigidbodyConstraints2D.None;
+        }
+    }
+    
 }
