a7fdaf5df5bb99752c59bf796f7823a7b343a0cd
RyanB565 <rblanco@mail.bradley.edu>
2026-09-16 14:22:00 -0500

    code re-writen

--- Assets/Scripts/SlashAttack.cs
+++ Assets/Scripts/SlashAttack.cs
@@ -5,34 +5,56 @@ using UnityEngine.InputSystem;
 public class SlashAttack : MonoBehaviour
 {
   
-    public float slashCooldown = 1f;
+    
     private InputAction slash;
+    private InputAction move;
     private Vector2 facingDirection = Vector2.right;
     public GameObject attackboxPrefab;
+
+    [SerializeField] private Animator slashAnim;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
       
         slash = InputSystem.actions.FindAction("Slash");
         slash.performed += Slash_performed;
-        
+
+        move = InputSystem.actions.FindAction("Move");
+        move.performed += Move_performed;
+
+
     }
+
+    private void Move_performed (InputAction.CallbackContext move)
+    {
+        Vector2 input = move.ReadValue<Vector2>();
+
+        if(input.sqrMagnitude > 0.01f)
+        {
+            facingDirection = input.normalized;
+        }
+    }
+
 
     private void Slash_performed (InputAction.CallbackContext context)
     {
-        StartCoroutine(Attackbox());
+        slashAnim.SetTrigger("Slash");
     }
 
-  
-    IEnumerator Attackbox()
+    public void SlashSpawn()
     {
-        Vector3 spawnPos = new Vector3(facingDirection.x, facingDirection.y, 0f); //player move direction
+        Vector3 spawnPos = transform.position + new Vector3(facingDirection.x, facingDirection.y, 0f) * 0.5f;
 
-        GameObject attackbox = Instantiate(attackboxPrefab, spawnPos, Quaternion.identity);
+        attackboxPrefab = Instantiate(attackboxPrefab, spawnPos, Quaternion.identity);
+    }
 
-        yield return new WaitForSeconds(0.15f);
-
-        Destroy(attackbox);
+    public void SlashEnd()
+    {
+        if (attackboxPrefab != null)
+        {
+            Destroy(attackboxPrefab);
+        }
     }
 
 }
